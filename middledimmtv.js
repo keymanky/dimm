0b495b14dabbe7d85d3adfbe688bd2b79b34763f
TU papa <gdf@gdf.mx>
2015-07-07 12:45:39 -0500

    Cambios segun observaciones 07-07-2015

--- middledimmtv.js
+++ middledimmtv.js
@@ -9,17 +9,17 @@ angular.module('middledimmtv',['ui.bootstrap'])
 	$scope.tab_activa = "";
 
 	$scope.slides.push({
-		image: 'imgs/circuito.jpg',
+		image: 'imgs/principal1.jpg',
 		text: 'Vea la demostración de nuestros proyectos.',
 		url: 'http://www.google.com.mx'
 	});
 	$scope.slides.push({
-		image: 'imgs/estudiantes2.jpg',
+		image: 'imgs/principal2.jpg',
 		text: 'Conozca quienes somos, a los que nos dedicamos y como integramos las tecnologias en su negocio.',
 		url: 'https://www.facebook.com/dimmTV'
 	});
 	$scope.slides.push({
-		image: 'imgs/silla.jpg',
+		image: 'imgs/principal3.jpg',
 		text: 'Vea el porque es importante mejorar sus procesos y reducir costos con estas tecnologias.',
 		url: 'https://www.flickr.com/'
 	});
